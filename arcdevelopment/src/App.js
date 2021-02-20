280d21e7b22e19453ca5c623e81b9f6f557b10ba
CalinChirvase <calin.chirvase@yahoo.com>
2021-02-19 23:05:19 -0500

    finish section 2

--- arcdevelopment/src/App.js
+++ arcdevelopment/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { BrowserRouter, BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import { ThemeProvider } from '@material-ui/styles'
 import theme from './components/Theme'
 import Header from './components/Header'
@@ -6,9 +8,38 @@ import Header from './components/Header'
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <Header />
-      </div>
+      <BrowserRouter>
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <div>Home</div>
+            </Route>
+            <Route exact path="/services">
+              <div>Services</div>
+            </Route>
+            <Route exact path="/customsoftware">
+              <div>Custom Software</div>
+            </Route>
+            <Route exact path="/mobileapps">
+              <div>Mobile Apps</div>
+            </Route>
+            <Route exact path="/websites">
+              <div>Websites</div>
+            </Route>
+            <Route exact path="/revolution">
+              <div>Revolution</div>
+            </Route>
+            <Route exact path="/about">
+              <div>About</div>
+            </Route>
+            <Route exact path="/contact">
+              <div>Contact</div>
+            </Route>
+            <Route exact path="/estimate">
+              <div>Estimate</div>
+            </Route>
+          </Switch>
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
--- arcdevelopment/src/components/Header.js
+++ arcdevelopment/src/components/Header.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import useScrollTrigger from '@material-ui/core/useScrollTrigger'
 import { makeStyles } from '@material-ui/styles'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import Button from '@material-ui/core/Button'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+
 import logo from '../assets/logo.svg'
 
 const ElevationScroll = (props) => {
@@ -23,18 +31,131 @@ const useStyles = makeStyles(theme => ({
     marginBottom: '3em'
   },
   logo: {
-    height: '7em'
+    height: '8em'
+  },
+  logoContainer: {
+    padding: 0,
+    '&:hover': {
+      backgroundColor: 'transparent'
+    }
+  },
+  tabContainer: {
+    marginLeft: 'auto'
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: '25px'
+  },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: '50px',
+    marginLeft: '50px',
+    marginRight: '25px',
+    height: '45px'
   }
 }))
 
 const Header = () => {
+  const [tabValue, setTabValue] = useState(0)
+  const [anchorEl, setAnchorEl] = useState(null)
+  const [open, setOpen] = useState(false)
+
   const classes = useStyles()
+
+  const handleTabChange = (event, value) => {
+    setTabValue(value)
+    setOpen(true)
+  }
+
+  const handleClose = (event) => {
+    setAnchorEl(null)
+    setOpen(false)
+  }
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  useEffect(() => {
+    if (window.location.pathname === '/' && tabValue !== 0) {
+      setTabValue(0)
+    } else if (window.location.pathname === '/services' && tabValue !== 1) {
+      setTabValue(1)
+    } else if (window.location.pathname === '/revolution' && tabValue !== 2) {
+      setTabValue(2)
+    } else if (window.location.pathname === '/about' && tabValue !== 3) {
+      setTabValue(3)
+    } else if (window.location.pathname === '/contact' && tabValue !== 4) {
+      setTabValue(4)
+    }
+  },[tabValue])
+
   return (
     <React.Fragment>
       <ElevationScroll>
         <AppBar position="fixed" color="primary">
           <Toolbar disableGutters>
-            <img className={classes.logo} alt="company logo" src={logo} />
+            <Button
+              component={Link}
+              to="/"
+              disableRipple
+              className={classes.logoContainer}
+              onClick={() => setTabValue(0)}
+            >
+              <img className={classes.logo} alt="company logo" src={logo} />
+            </Button>
+            <Tabs 
+              value={tabValue}
+              onChange={handleTabChange}
+              className={classes.tabContainer}
+              indicatorColor="primary"
+              >
+              <Tab 
+                component={Link} to="/"
+                className={classes.tab}
+                label="Home" />
+              <Tab 
+                onClick={(event) => handleClick(event)}
+                aria-owns={anchorEl ? "simple-menu" : undefined}
+                aria-haspopup={anchorEl ? true : undefined}
+                component={Link}
+                to="/services"
+                className={classes.tab}
+                label="Services" />
+              <Tab 
+                component={Link}
+                to="/revolution"
+                className={classes.tab}
+                label="The Revolution" />
+              <Tab 
+                component={Link}
+                to="/about" className={classes.tab}
+                label="About Us" />
+              <Tab 
+                component={Link}
+                to="/contact"
+                className={classes.tab}
+                label="Contact Us" />
+            </Tabs>
+            <Button variant="contained" color="secondary" className={classes.button}>
+              Free Estimate
+            </Button>
+            <Menu id="simple-menu"
+            anchor={anchorEl}
+            open={open}
+            onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>
+                Custom Software Development
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                Mobile App Development
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                Website Development
+              </MenuItem>
+            </Menu>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
